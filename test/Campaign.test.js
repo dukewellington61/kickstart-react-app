77e09828e4c4caeff5da5bfaa96a30b839882829
akoeth-portfolio <dukewellington61@googlemail.com>
2020-10-06 01:06:10 +0200

    Lecture 141

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const ganache = requires("ganache-cli");
+const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
@@ -36,9 +36,57 @@ beforeEach(async () => {
 
   // instructs web3 to create a javascript representation of the instance of the Campaign contract which is beeing instantiated using the factory contract
   // which has been deployed to the blockchain with the address campaignAddress
-  // that we can then interact with said javascript representation
+  // than we can then interact with said javascript representation
   campaign = await new web3.eth.Contract(
     JSON.parse(compiledCampaign.interface),
     campaignAddress
   );
+});
+
+describe("Campaign", () => {
+  it("deploys a factory and a campaign", () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+
+  it("marks caller as the campaing manager", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.strictEqual(accounts[0], manager);
+  });
+
+  // tests if contributers are beeing entered in the approvers mapping
+  it("allows people to contribute money and marks them as approvers", async () => {
+    await campaign.methods.contribute().send({
+      value: "200",
+      from: accounts[1],
+    });
+    // we have marked the mapping 'approvers' as public, thatswhy solidity has created a getter
+    // which takes an address as an argument and returns true if this address is in the mapping
+    const isContributer = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributer);
+  });
+
+  it("requires a minimum contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: "99",
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("fails if a minimum contribution isn't met", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: "99",
+        from: accounts[1],
+      });
+      assert(true);
+    } catch (err) {
+      assert(false);
+    }
+  });
 });
