5ed5dc7e16256219b9c0d9ad172b6ec0eb39dbf2
akoeth-portfolio <dukewellington61@googlemail.com>
2020-10-19 12:50:47 +0200

    lecture 197

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,15 +1,56 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
+import Campaign from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
+import { Router } from "../routes";
 
 class ContributeForm extends Component {
+  state = {
+    value: "",
+    errorMessage: "",
+    loading: false,
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+
+    this.setState({ loading: true, errorMessage: "" });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, "ether"),
+      });
+
+      // reroutes to the url we are currently on which basically refreshes
+      // this url so the updated values are beeing displayed
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false, value: "" });
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
-          <Input label="ether" labelPosition="right" />
+          <Input
+            value={this.state.value}
+            onChange={(e) => this.setState({ value: e.target.value })}
+            label="ether"
+            labelPosition="right"
+          />
         </Form.Field>
-        <Button>Contribute!</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute!
+        </Button>
       </Form>
     );
   }
